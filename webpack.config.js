009562b5db4aa18ee3230ea2dca91088a74f9688
Nawawish <hydrokidz@gmail.com>
2018-04-29 22:10:01 +0700

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,5 @@ module.exports = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
-			}
-		]
 	}
 };
