1f72ec0ed5a2c87eb8959eea2c05c92fb3ce4ba4
Nawawish <hydrokidz@gmail.com>
2018-04-29 21:56:16 +0700

    Complete Asset Management lesson

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon-magnifier-32x32.svg';
+import Data from './data.xml';
 
 function component() {
 	var element = document.createElement('div');
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
+
+	var myIcon = new Image();
+	myIcon.src = Icon;
+
+	element.appendChild(myIcon);
+
+	console.log(Data);
 
 	return element;
 }
